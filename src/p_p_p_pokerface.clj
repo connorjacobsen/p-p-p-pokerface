4e9839589a2361c715b2cc06950c86a79914f6c3
Connor Jacobsen <jacobsen.connor@gmail.com>
2015-06-13 01:16:43 -0700

    hooray clojure!

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,83 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
+(defn hand [a-hand]
+  a-hand)
+
+(defn card [a-card]
+  a-card)
+
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (replacements r))))
+
+(defn ranks [hand]
+  (map rank hand))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn suits [hand]
+  (map suit hand))
+
+(defn rank-frequencies [hand]
+  (vals (frequencies (ranks hand))))
+
+(defn sorted-rank-frequencies [hand]
+  (sort (rank-frequencies hand)))
+
+(defn n-of-a-kind [hand]
+  (apply max (rank-frequencies hand)))
+
+(defn n-of-a-kind? [n hand]
+  (if (>= (n-of-a-kind hand) n)
+    true
+    false))
+
+(defn high-card? [hand]
+  true)
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (first (vals (frequencies (suits hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (if (= '(2 3) (sorted-rank-frequencies hand))
+    true
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sorted-rank-frequencies hand)
+     '(1 2 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (sort (ranks hand))
+        n (first sorted-ranks)
+        sorted-ranks-ace (sort (replace {14 1} (ranks hand)))
+        straight (fn [first-rank] (range first-rank (+ first-rank 5)))]
+    (or (= sorted-ranks (straight n)) (= sorted-ranks-ace (straight 1)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        matching-checkers (filter #((first %1) hand) checkers)
+        matching-values (map #(second %1) matching-checkers)]
+    (apply max matching-values)))
